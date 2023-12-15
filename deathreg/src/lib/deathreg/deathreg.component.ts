980c74ef18355b85237ee454ff2a66d8cf34a6a5
Allan-Ngugi <blessedallan1@gmail.com>
2023-12-15 23:43:43 +0300

    ✨ Deathreg lib

---
+++ deathreg/src/lib/deathreg/deathreg.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'reg-angular-deathreg',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './deathreg.component.html',
+  styleUrl: './deathreg.component.css',
+})
+export class DeathregComponent {}
