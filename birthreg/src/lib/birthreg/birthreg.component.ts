4064365734e7a95e833730fd8b0d18d8b5527bf4
Allan-Ngugi <blessedallan1@gmail.com>
2023-12-15 23:44:37 +0300

    ✨ Birthreg lib

---
+++ birthreg/src/lib/birthreg/birthreg.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'reg-angular-birthreg',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './birthreg.component.html',
+  styleUrl: './birthreg.component.css',
+})
+export class BirthregComponent {}
