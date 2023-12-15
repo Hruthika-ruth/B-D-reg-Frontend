2cd01be366b5fb98e8002d814b9b16510790c530
Allan-Ngugi <blessedallan1@gmail.com>
2023-12-16 00:29:56 +0300

    ✨ Routing files

---
+++ sharedd/src/lib/sharedd/sharedd.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'reg-angular-sharedd',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './sharedd.component.html',
+  styleUrl: './sharedd.component.css',
+})
+export class ShareddComponent {}
