2e0bd0bc56bf9db62a5474cf686c7e636df686a3
Allan-Ngugi <blessedallan1@gmail.com>
2023-12-19 20:46:05 +0300

    📝 Updated the death and birth config files

--- birthreg/src/lib/birthreg/birthreg.component.ts
+++ birthreg/src/lib/birthreg/birthreg.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'reg-angular-birthreg',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './birthreg.component.html',
   styleUrl: './birthreg.component.css',
 })
--- deathreg/src/lib/deathreg/deathreg.component.ts
+++ deathreg/src/lib/deathreg/deathreg.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'reg-angular-deathreg',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './deathreg.component.html',
   styleUrl: './deathreg.component.css',
 })
