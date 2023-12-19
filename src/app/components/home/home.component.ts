cefceac29e1f8dbba4be014c2549451f8aee4a39
Allan-Ngugi <blessedallan1@gmail.com>
2023-12-19 20:43:48 +0300

    🚧  Work in Progress

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  FormGroup,
+  ReactiveFormsModule,
+  FormControl,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'reg-angular-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  title = 'reg-angular';
+  applyForm = new FormGroup({
+    fullName: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    Address: new FormControl(''),
+    message: new FormControl(''),
+  });
+
+  get email() {
+    return this.applyForm.get('email');
+  }
+}
